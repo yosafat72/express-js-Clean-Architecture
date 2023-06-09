c9250091612401760795d893745f5898a3bb17d6
Yosafat Hermawan S <yosafat.h.s@gmail.com>
2023-06-09 12:09:04 +0700

    update email validation

--- src/adapters/controllers/UserController.ts
+++ src/adapters/controllers/UserController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { UserService } from '../../application/usecases/UserService';
+import { validateEmail } from '../../infrastructure/utility/EmailValidator';
 
 export class UserController{
     private userService: UserService;
@@ -19,6 +20,11 @@ export class UserController{
     }
 
     public createUser(req: Request, res: Response): void {
+        if(!validateEmail(req.body.email)){
+            res.status(400).json({ error: 'Email is not valid' });
+            return
+        }
+
         const userData = req.body;
         const newUser = this.userService.createUser(userData);
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import router from './routes/router';
+import bodyParser from 'body-parser';
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(router);
 
 export default { app }
