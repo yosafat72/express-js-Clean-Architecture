7639ef5ac7b5e09390a2cfc231aff1d8c741fb9e
Yosafat Hermawan S <yosafat.h.s@gmail.com>
2023-06-09 11:05:09 +0700

    delete node module and dist

--- dist/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const main_1 = __importDefault(require("./main"));
-main_1.default.app.listen(3000, () => {
-    console.log('Server started on port 3000');
-});
