73b65b0dfc6aaeabaf4f37586949c533eebb21a8
Yosafat Hermawan S <yosafat.h.s@gmail.com>
2023-06-12 14:41:59 +0700

    update koneksi dengan dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import express from 'express';
 import router from './routes/router';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import path from 'path';
+
+const envPath = path.resolve(__dirname, 'infrastructure', 'config', '.env');
+dotenv.config({ path: envPath });
 
 const app = express();
 
